4672a41e2163c3a236cf7ad43fd52838537095e7
zky2824193947 <2824193947@qq.com>
2021-05-16 21:39:24 +0800

    添加 input 测试用例

--- src/input.vue
+++ src/input.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="wrapper">
-    <input type="text" :value="value" :disabled="disabled" :readonly="readonly" :class="{'error': error}">
+    <input type="text" :value="value" :disabled="disabled" :readonly="readonly" :class="{'error': error}"
+           @change="$emit('change', $event)"
+           @input="$emit('input', $event)"
+           @focus="$emit('focus', $event)"
+           @blur="$emit('blur', $event)"
+    >
     <template v-if="error">
       <icon name="error" class="icon-error"></icon>
       <span class="errorMessage">{{error}}</span>
@@ -12,10 +17,11 @@
 import Icon from './icon'
 
 export default {
+  name: "g-input",
+  emits: ['change'],
   components: {
     'icon': Icon
   },
-  name: "g-input",
   props: {
     value: {
       type: String
---
+++ test/input.test.js
@@ -0,0 +1,79 @@
+import Button from "../src/button";
+
+const expect = chai.expect;
+import Vue from 'vue'
+import Input from '../src/input'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Input', () => {
+  // BDD 行为驱动测试
+  it('存在.', () => {
+    expect(Input).to.be.ok
+  })
+
+  describe('props', () => {
+    it('接收 value', () => {
+      const Constructor = Vue.extend(Input)
+      const vm = new Constructor({
+        propsData: {
+          value: '1234'
+        }
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.value).to.equal('1234')
+      vm.$destroy()
+    })
+    it('接收 disabled', () => {
+      const Constructor = Vue.extend(Input)
+      const vm = new Constructor({
+        propsData: {
+          disabled: true
+        }
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.disabled).to.equal(true)
+      vm.$destroy()
+    })
+    it('接收 readonly', () => {
+      const Constructor = Vue.extend(Input)
+      const vm = new Constructor({
+        propsData: {
+          readonly: true
+        }
+      }).$mount()
+      const inputElement = vm.$el.querySelector('input')
+      expect(inputElement.readOnly).to.equal(true)
+      vm.$destroy()
+    })
+    it('接收 error', () => {
+      const Constructor = Vue.extend(Input)
+      const vm = new Constructor({
+        propsData: {
+          error: '你错了'
+        }
+      }).$mount()
+      const useElement = vm.$el.querySelector('use')
+      expect(useElement.getAttribute('xlink:href')).to.equal('#i-error')
+      const errorMessage = vm.$el.querySelector('.errorMessage')
+      expect(errorMessage.innerText).to.equal('你错了')
+      vm.$destroy()
+    })
+  })
+  describe('事件', () => {
+    it('支持  事件', () => {
+      ['change', 'input', 'focus', 'blur'].forEach((item) => {
+        const Constructor = Vue.extend(Input)
+        const vm = new Constructor({}).$mount()
+        const callback = sinon.fake()
+        vm.$on(item, callback)
+        // 触发input的change事件
+        var event = new Event(item)
+        let inputElement = vm.$el.querySelector('input')
+        inputElement.dispatchEvent(event)
+        expect(callback).to.have.been.calledWith(event)
+      })
+    })
+  })
+})
