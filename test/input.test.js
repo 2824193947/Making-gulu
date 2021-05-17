bbda4ea59eebc70047e1e338f178f215ef36497f
zky2824193947 <2824193947@qq.com>
2021-05-17 10:43:03 +0800

    让 input.vue 支持v-model

--- test/input.test.js
+++ test/input.test.js
@@ -70,9 +70,12 @@ describe('Input', () => {
         vm.$on(item, callback)
         // 触发input的change事件
         var event = new Event(item)
+        Object.defineProperty(event, 'target', {
+          value: {value: 'hi'}, enumerable: true
+        })
         let inputElement = vm.$el.querySelector('input')
         inputElement.dispatchEvent(event)
-        expect(callback).to.have.been.calledWith(event)
+        expect(callback).to.have.been.calledWith('hi')
       })
     })
   })
