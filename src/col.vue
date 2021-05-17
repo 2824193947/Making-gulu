e6befdf7b3b81566d9aeaa00312847c5a34540c0
zky2824193947 <2824193947@qq.com>
2021-05-17 22:58:27 +0800

    重构 row和col

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,6 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-       :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
-    <div style="border: 1px solid green; height: 100px;">
+  <div class="col" :class="colClass" :style="colStyle">
       <slot></slot>
-    </div>
   </div>
 </template>
 
@@ -12,7 +9,22 @@ export default {
   name: "g-col",
   data () {
     return {
-      gutter: 0
+      gutter: 0,
+    }
+  },
+  computed: {
+    colClass () {
+      // return [span && `col-${span}`, offset && `offset-${offset}`]
+      // 因为span会识别为变量两个方法解决
+      return '[span && `col-${span}`, offset && `offset-${offset}`]'
+      // 或者声明这两个变量 let span, offset = this
+
+    },
+    colStyle () {
+      return {
+        paddingLeft: this.gutter/2+'px',
+        paddingRight: this.gutter/2+'px'
+      }
     }
   },
   props: {
--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
+  <div class="row" :style="rowStyle">
     <slot></slot>
   </div>
 </template>
@@ -12,9 +12,17 @@ export default {
       type: [Number, String]
     }
   },
+  computed: {
+    rowStyle () {
+      let { gutter } = this
+      return {
+        marginLeft: -gutter/2+'px',
+        marginRight: -gutter/2+'px'
+      }
+    }
+  },
   mounted () {
     // 获取到row中的col
-    console.log(this.$children)
     this.$children.forEach((vm => {
       vm.gutter = this.gutter
     }))
