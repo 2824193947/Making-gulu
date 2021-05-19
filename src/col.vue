3ef9deb14c730e91e2fee744d5447f2ddf37e29c
zky2824193947 <2824193947@qq.com>
2021-05-19 08:18:26 +0800

    初步实现 响应式 @media 分别有 phone,ipad,pc,narrowpc,waidepc模式

--- src/col.vue
+++ src/col.vue
@@ -14,11 +14,92 @@ export default {
       gutter: 0,
     }
   },
+  props: {
+    span: {
+      type: [Number, String],
+    default: 24
+    },
+    offset: {
+      type: [Number, String]
+    },
+    phone: {
+      type: Object,
+      validator ( value ) {
+        let keys = Object.keys(value)
+        let valid = true
+        keys.forEach((key) => {
+          if (!['span', 'offset'].includes(key)) {
+            valid = false
+          }
+        })
+        return valid
+      }
+    },
+    ipad: {
+      type: Object,
+      validator ( value ) {
+        let keys = Object.keys(value)
+        let valid = true
+        keys.forEach((key) => {
+          if (!['span', 'offset'].includes(key)) {
+            valid = false
+          }
+        })
+        return valid
+      }
+    },
+    narrowPc: {
+      type: Object,
+      validator ( value ) {
+        let keys = Object.keys(value)
+        let valid = true
+        keys.forEach((key) => {
+          if (!['span', 'offset'].includes(key)) {
+            valid = false
+          }
+        })
+        return valid
+      }
+    },
+    pc: {
+      type: Object,
+      validator ( value ) {
+        let keys = Object.keys(value)
+        let valid = true
+        keys.forEach((key) => {
+          if (!['span', 'offset'].includes(key)) {
+            valid = false
+          }
+        })
+        return valid
+      }
+    },
+    widePc: {
+      type: Object,
+      validator ( value ) {
+        let keys = Object.keys(value)
+        let valid = true
+        keys.forEach((key) => {
+          if (!['span', 'offset'].includes(key)) {
+            valid = false
+          }
+        })
+        return valid
+      }
+    }
+  },
   computed: {
     colClass () {
-      let { span, offset } = this
-      return [span && `col-${span}`, offset && `offset-${offset}`]
-
+      let {span, offset, phone, ipad, narrowPc, pc, widePc} = this
+      return [
+        span && `col-${span}`,
+        offset && `offset-${offset}`,
+        ...(phone ? [`col-phone-${phone.span}`, `offset-phone-${phone.offset}`] : []),
+        ...(ipad ? [`col-ipad-${ipad.span}`, `offset-ipad-${ipad.offset}`] : []),
+        ...(narrowPc ? [`col-narrowPc-${phone.span}`, `offset-narrowPc-${narrowPc.offset}`] : []),
+        ...(pc ? [`col-pc-${pc.span}`, `offset-pc-${pc.offset}`] : []),
+        ...(widePc ? [`col-widePc-${widePc.span}`, `offset-widePc-${widePc.offset}`] : [])
+      ]
     },
     colStyle () {
       return {
@@ -26,15 +107,6 @@ export default {
         paddingRight: this.gutter/2+'px'
       }
     }
-  },
-  props: {
-    span: {
-      type: [Number, String],
-      default: 24
-    },
-    offset: {
-      type: [Number, String]
-    },
   }
 }
 </script>
@@ -52,6 +124,76 @@ export default {
   @for $n from 1 through 24 {
     &.#{$class}#{$n} {
       margin-left: ($n / 24) * 100%;
+    }
+  }
+  @media (max-width: 576px) {
+    $class: col-phone-;
+    @for $n from 1 through 24 {
+      &.#{$class}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class: offset-phone-;
+    @for $n from 1 through 24 {
+      &.#{$class}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 577px) and (max-width: 768px) {
+    $class: col-ipad-;
+    @for $n from 1 through 24 {
+      &.#{$class}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class: offset-ipad-;
+    @for $n from 1 through 24 {
+      &.#{$class}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 769px) and (max-width: 992px) {
+    $class: col-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class: offset-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 993px) and (max-width: 1200px) {
+    $class: col-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class: offset-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
+    }
+  }
+  @media (min-width: 1201px) {
+    $class: col-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+    $class: offset-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class}#{$n} {
+        margin-left: ($n / 24) * 100%;
+      }
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -43,6 +43,7 @@ export default {
 <style lang="scss" scoped>
 .row {
   display: flex;
+  flex-wrap: wrap;
   &.align-right {
     justify-content: flex-end;
   }
