cb9aee665a9a07047f52f7ded58c108b12326328
zky2824193947 <2824193947@qq.com>
2021-05-22 14:14:03 +0800

    实现进入动画

--- src/toast.vue
+++ src/toast.vue
@@ -86,7 +86,18 @@ $font-size: 14px;
 $toast-height: 35px;
 $bg-color: rgba(0,0,0,0.74);
 
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+    transform: translateY(-100%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
 .toast {
+  animation: fade-in 0.5s;
   position: fixed;
   left: 50%;
   display: flex;
