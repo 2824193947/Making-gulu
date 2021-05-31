18e4467843c10ef2bdc63b1ffc540fc491676a47
zky2824193947 <2824193947@qq.com>
2021-05-31 16:42:22 +0800

    添加一个测试用例(有一个警告没有解决)， 还有一个想不出测试方法

---
+++ test/popover.test.js
@@ -0,0 +1,38 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Popover from '../src/popover'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Popover', () => {
+  // BDD 行为驱动测试
+
+  it('存在.', () => {
+    expect(Popover).to.be.ok
+  })
+  it('可以设置position.', (done) => {
+    Vue.component('g-popover', Popover)
+    const div1 = document.createElement('div')
+    document.body.appendChild(div1)
+    div1.innerHTML = `
+    <g-popover position="bottom" ref="a">
+      <template slot="content">
+      弹出内容
+      </template>
+      <button>点我</button>
+    </g-popover>
+    `
+    const vm = new Vue({
+      el: div1
+    })
+    vm.$el.querySelector('button').click()
+    vm.$nextTick(() => {
+      const {contentWrapperRef} = vm.$refs.a.$refs
+      expect(contentWrapperRef.classList.contains('position-bottom')).to.be.true
+      done()
+      vm.$el.remove()
+      vm.$destroy()
+    })
+  })
+})
