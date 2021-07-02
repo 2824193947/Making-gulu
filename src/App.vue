b0d650649ee171fd0d3a0114f5b4d02695bf0f92
zky2824193947 <2824193947@qq.com>
2021-07-02 16:10:14 +0800

    添加 vue-cli (本项目是parcel构建的项目)

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app">
+    123123
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style scoped>
+.app {
+  margin: 20px;
+}
+</style>
