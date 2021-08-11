61da8daca4b97610a63c96330d5250655c979c91
zky2824193947 <2824193947@qq.com>
2021-08-11 09:58:18 +0800

    添加 cascader 组件, 添加parcel的启动命令,因为引入vuecli有问题

---
+++ src/cascader-item.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="CascaderItem">
+    {{sourceItem.name}}
+    <g-cascaderItem
+       v-for="item in sourceItem.children"
+       v-if="sourceItem.children"
+       :sourceItem="item"
+    ></g-cascaderItem>
+  </div>
+</template>
+
+<script>
+import CascaderItem from './cascader-item'
+
+export default {
+  name: "g-cascaderItem",
+  components: {
+    'g-cascaderItem': CascaderItem
+  },
+  props: {
+    sourceItem: {
+      type: Object
+    }
+  },
+  mounted() {
+    console.log(this.sourceItem)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
